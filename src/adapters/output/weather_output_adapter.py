040afac2f601c1d1356c3621cb4e973971ddac22
Manuel Novas <enam_rda@yahoo.com.mx>
2026-04-01 02:13:20 +0000

    Add cache to get_timeline

--- src/adapters/output/weather_output_adapter.py
+++ src/adapters/output/weather_output_adapter.py
@@ -1,10 +1,19 @@
+from dotenv import load_dotenv
+from functools import lru_cache
+from os import getenv
 from requests import get
 
 from src.application.ports.output import WeatherOutputPort
 
+load_dotenv()
+
 
 class WeatherOutputAdapter(WeatherOutputPort):
+    @lru_cache(maxsize=128)
     def get_timeline(self, location: str) -> dict:
-        url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}"
+        key = getenv("VISUAL_CROSSING_KEY")
+        url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}?key={key}"
         response = get(url)
+        if response.status_code != 200:
+            raise Exception(code=response.status_code, message=response.text)
         return response.json()
